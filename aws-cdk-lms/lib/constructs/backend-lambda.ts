d758abbcc255b58e8413087ab757fb36620e44b1
saifalmaliki <saif.wsm@gmail.com>
2025-01-08 11:25:40 +0100

    update s3 to accept videos

--- aws-cdk-lms/lib/constructs/backend-lambda.ts
+++ aws-cdk-lms/lib/constructs/backend-lambda.ts
@@ -23,6 +23,7 @@ export class BackendLambda extends Construct {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'index.handler',
       timeout: cdk.Duration.seconds(30),
+      memorySize: 1024,
       code: lambda.Code.fromAsset(path.join(__dirname, '../../../server/dist')),
       environment: {
         NODE_ENV: 'production',
@@ -37,6 +38,25 @@ export class BackendLambda extends Construct {
     // Grant DynamoDB Permissions
     this.lambdaFunction.role?.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
+    );
+
+    // Grant S3 Permissions
+    this.lambdaFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          's3:PutObject',
+          's3:GetObject',
+          's3:DeleteObject',
+          's3:ListBucket',
+          's3:GetObjectTagging',
+          's3:PutObjectTagging'
+        ],
+        resources: [
+          `arn:aws:s3:::${props.s3BucketName}`,
+          `arn:aws:s3:::${props.s3BucketName}/*`
+        ],
+      })
     );
   }
 }
--- aws-cdk-lms/lib/constructs/s3-bucket.ts
+++ aws-cdk-lms/lib/constructs/s3-bucket.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class LmsS3Bucket extends Construct {
   public readonly bucket: s3.Bucket;
@@ -14,9 +14,56 @@ export class LmsS3Bucket extends Construct {
 
     // Create the S3 bucket
     this.bucket = new s3.Bucket(this, 'LmsS3Bucket', {
-      bucketName: bucketName, // Use the unique bucket name
+      bucketName: bucketName,
       versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Use RETAIN for production
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      cors: [
+        {
+          allowedHeaders: ['*'],
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.PUT,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.DELETE,
+            s3.HttpMethods.HEAD,
+          ],
+          allowedOrigins: ['*'],  // Allow all origins for development
+          exposedHeaders: [
+            'ETag',
+            'x-amz-meta-custom-header',
+            'x-amz-server-side-encryption',
+            'x-amz-request-id',
+            'x-amz-id-2'
+          ],
+          maxAge: 3000,
+        },
+      ],
+      // Allow public read access for development
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false
+      })
     });
+
+    // Add bucket policy to allow necessary actions
+    const bucketPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        's3:PutObject',
+        's3:GetObject',
+        's3:DeleteObject',
+        's3:ListBucket'
+      ],
+      resources: [
+        this.bucket.bucketArn,
+        `${this.bucket.bucketArn}/*`
+      ],
+      principals: [new iam.AnyPrincipal()]
+    });
+
+    this.bucket.addToResourcePolicy(bucketPolicy);
   }
 }
